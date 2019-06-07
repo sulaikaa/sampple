0860f9676410970a1a20630baca69484d05275b9
sulaikaa <sulaika.shaik@letsgettin.com>
2019-06-07 12:48:32 +0530

    updated dao class

--- sampple/src/main/java/com/qb/phd/pharmacy/Dao/ReggDao.java
+++ sampple/src/main/java/com/qb/phd/pharmacy/Dao/ReggDao.java
@@ -1,9 +1,10 @@
 package com.qb.phd.pharmacy.Dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.qb.phd.pharmacy.model.Regg;
-
+@Repository
 public interface ReggDao extends JpaRepository<Regg,Long>{
 
 }
