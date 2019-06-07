d6e598f1f6b5c7206150e9d3ff9e0bad7ea19e93
sulaikaa <sulaika.shaik@letsgettin.com>
2019-06-07 12:55:47 +0530

    added controller class

---
+++ sampple/src/main/java/com/qb/phd/pharmacy/Controller/ReggController.java
@@ -0,0 +1,23 @@
+package com.qb.phd.pharmacy.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.qb.phd.pharmacy.Dao.ReggDao;
+import com.qb.phd.pharmacy.model.Regg;
+
+@RestController
+@RequestMapping("/api/auth")
+public class ReggController {
+
+	@Autowired
+	ReggDao rd;
+	
+	@PostMapping("/create")
+	public Regg createuser(@RequestBody Regg r) {
+		return rd.save(r);
+	}
+}
