89c678bb45a7722db2fdeb6f13eac9c5d4b3c364
sulaikaa <sulaika.shaik@letsgettin.com>
2019-06-07 12:43:09 +0530

    added dao class

---
+++ sampple/src/main/java/com/qb/phd/pharmacy/Dao/ReggDao.java
@@ -0,0 +1,9 @@
+package com.qb.phd.pharmacy.Dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.qb.phd.pharmacy.model.Regg;
+
+public interface ReggDao extends JpaRepository<Regg,Long>{
+
+}
