8839bc88616f94007700b1e2644b2d09af6c56d2
sulaikaa <sulaika.shaik@letsgettin.com>
2019-06-07 12:33:52 +0530

    first commit

---
+++ sampple/src/main/java/com/qb/phd/pharmacy/SamppleApplication.java
@@ -0,0 +1,13 @@
+package com.qb.phd.pharmacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamppleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamppleApplication.class, args);
+	}
+
+}
---
+++ sampple/src/test/java/com/qb/phd/pharmacy/SamppleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.qb.phd.pharmacy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SamppleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
